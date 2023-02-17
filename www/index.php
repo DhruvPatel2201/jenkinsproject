a3de6aee8ad81b71d88f5cfdb1601b43017cd971
DhruvPatel2201 <105780977+DhruvPatel2201@users.noreply.github.com>
2023-02-17 14:22:54 +0530

    Update index.php

--- www/index.php
+++ www/index.php
@@ -20,7 +20,7 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<?php if($_ENV["HOSTNAME"]) {?><h3>My is aws  Ccloud<?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
+	<?php if($_ENV["HOSTNAME"]) {?><h3>My is aws  Dcloud<?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
 	foreach($_ENV as $key => $value) {
